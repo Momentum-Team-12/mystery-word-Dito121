cab81def5123e83acb9a47438170e8c662fbdde0
Gerardo Ayes <gerardoayes92@gmail.com>
2022-04-22 16:40:48 -0400

    implemented get random word function

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,17 @@
+import random
+
+
 def play_game():
-    pass
+    random_word = get_random_word()
+    print(random_word)
+
+
+def get_random_word():
+    with open('words.txt', 'r') as f:
+        lines = f.readlines()
+        line = lines[random.randint(0, len(lines) - 1)]
+        line = line.split()
+        return line[random.randint(0, len(line) - 1)]
 
 
 if __name__ == "__main__":
